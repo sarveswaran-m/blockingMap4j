214bad4cc2715a5d351c1a2f0fe739b31d8775e5
Sarveswaran M <sarveswaran.ms@gmail.com>
2018-01-06 18:56:22 +0530

    Introducing unit Test for bug fix (Bug ID : #1)

--- src/test/java/org/thavam/util/concurrent/blockingMap/BlockingMapTest.java
+++ src/test/java/org/thavam/util/concurrent/blockingMap/BlockingMapTest.java
@@ -5,29 +5,184 @@
  */
 package org.thavam.util.concurrent.blockingMap;
 
-import java.util.concurrent.TimeUnit;
-import junit.framework.TestCase;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.FutureTask;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 /**
  *
  * @author emeensa
  */
-public class BlockingMapTest extends TestCase {
-    
-    public BlockingMapTest(String testName) {
-        super(testName);
-    }
-    
-    @Override
-    protected void setUp() throws Exception {
-        super.setUp();
-        
-    }
-    
-    @Override
-    protected void tearDown() throws Exception {
-        super.tearDown();
+public class BlockingMapTest {
+
+    //thread names
+    public static final String CONSUMER_1 = "consumer-1";
+    public static final String CONSUMER_2 = "consumer-2";
+    public static final String PRODUCER_1 = "producer-1";
+    public static final String PRODUCER_2 = "producer-2";
+   
+    //map containing conditions
+    static volatile BlockingMap<Integer, String> blockingMap = null;
+
+   
+    //value returned on interruption
+    static final String INTERRUPTED = "INTERRUPTED";
+    static final String EMTPY_STR = "";
+    static ExecutorService executor;
+    //blocking thread
+    private static Callable producer1, producer2;
+    private static FutureTask<String> consumerTask1, consumerTask2;
+
+    static void printDecoratedMessage(String msg) {
+        Logger.getLogger(BlockingMapTest.class.getName()).log(
+                Level.INFO, Thread.currentThread().getName() + " : " + msg);
     }
 
-    
+    public BlockingMapTest() {
+        blockingMap = new BlockingHashMap<>();
+        executor = Executors.newCachedThreadPool();
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+
+        //first consumer thread
+        consumerTask1 = new FutureTask<String>(new Callable<String>() {
+            @Override
+            public String call() {
+                String value = null;
+                try {
+                    value = blockingMap.take(1);
+                } catch (InterruptedException ex) {
+                    printDecoratedMessage("Exception on consumer 1");
+                    value = INTERRUPTED;
+                }
+                return value;
+            }
+        });
+
+        //second consumer thread
+        consumerTask2 = new FutureTask<String>(new Callable<String>() {
+            @Override
+            public String call() {
+                String value = null;
+                try {
+                    value = blockingMap.take(Integer.MIN_VALUE);
+                } catch (InterruptedException ex) {
+                    printDecoratedMessage("Exception on consumer 2");
+                    value = INTERRUPTED;
+                }
+                return value;
+            }
+        });
+
+        //producer thread 1
+        producer1 = new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                String value = null;
+                try {
+                    Thread.sleep(5000);
+                    value = blockingMap.put(1, "one");
+                } catch (InterruptedException e) {
+                    //interrupted
+                }
+                return value;
+            }
+        };
+
+        //producer thread 2
+        producer2 = new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                String value = null;
+                try {
+                    Thread.sleep(5000);
+                    value = blockingMap.put(2, "two");
+                } catch (InterruptedException e) {
+                    //interrupted
+                }
+                return value;
+            }
+        };
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+        executor.shutdown();
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of clear to interrupt blocked threads both consumers wait for their
+     * keys of interest before producers start producing. This is ensured by
+     * making producers sleep before production.
+     *
+     * consumer2 waits on non-existent key, there it will block for-ever on
+     * clear, consumer2 should be interrupted
+     *
+     * Bug Id #1 
+     * https://github.com/sarveswaran-m/blockingMap4j/issues/1
+     */
+    @Test
+    public void testClear() throws InterruptedException, ExecutionException {
+        /**
+         * log messages introduced to understand thread interaction. should be
+         * removed.
+         *
+         * Assertion for marker value on blocked consumer or expecting
+         * Interrupted exception would suffice
+         */
+        printDecoratedMessage("Starting consumers");
+
+        executor.submit(consumerTask1);
+        executor.submit(consumerTask2);
+
+        printDecoratedMessage("Starting Consumers");
+        executor.submit(producer1);
+        executor.submit(producer2);
+
+        executor.submit(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    printDecoratedMessage("waiting for results on different thread:");
+                    printDecoratedMessage("consumerTask1.get() = " + consumerTask1.get());
+                    printDecoratedMessage("consumerTask2.get() = " + consumerTask2.get());
+                } catch (InterruptedException ex) {
+                    printDecoratedMessage("Interrupted : consumerTask2.get() = ");
+                } catch (ExecutionException ex) {
+                    //Logger.getLogger(BlockingMapTest.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        });
+
+        printDecoratedMessage("waiting for producers to complete prodcution");
+        Thread.sleep(12000);
+
+        printDecoratedMessage("clearing synchronizer :: This will interrupt any waiting consumer");
+        blockingMap.clear();
+
+        assertTrue(consumerTask1.get().equalsIgnoreCase("one")
+                && (consumerTask2.get().equalsIgnoreCase(INTERRUPTED)));
+
+    }
+
 }
